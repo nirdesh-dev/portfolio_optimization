21662e98a4c3b0191fac674d7a53d952f0d5eac5
nirdesh-dev <nbhattarai.work@gmail.com>
2025-05-12 06:41:47 +0545

    Fmt changes

--- src/data/fetch_data.rs
+++ src/data/fetch_data.rs
@@ -1,22 +1,23 @@
-use std::collections::HashMap;
-use anyhow::Result;
-use yahoo_finance_api as yahoo;
-use chrono::NaiveDate;
 use crate::data::yahoo_periods::{Interval, Range};
+use anyhow::Result;
+use chrono::NaiveDate;
+use std::collections::HashMap;
+use yahoo_finance_api as yahoo;
 
 pub async fn fetch_close_prices_range(
     provider: &yahoo::YahooConnector,
-    symbols: &[&str], interval: Interval, range: Range
+    symbols: &[&str],
+    interval: Interval,
+    range: Range,
 ) -> Result<Vec<HashMap<String, Vec<f32>>>> {
-
     let mut close_prices = Vec::new();
     for &symbol in symbols {
-        let response = provider.get_quote_range(
-            symbol,
-            interval.as_str(),
-            range.as_str()
-        ).await?;
-        let quotes_close_prices: Vec<f32> = response.quotes().unwrap()
+        let response = provider
+            .get_quote_range(symbol, interval.as_str(), range.as_str())
+            .await?;
+        let quotes_close_prices: Vec<f32> = response
+            .quotes()
+            .unwrap()
             .into_iter()
             .map(|quote| quote.close as f32)
             .collect();
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 mod data;
 mod optimizer;
 
-use yahoo_finance_api as yahoo;
-use anyhow::Result;
 use crate::data::fetch_data::fetch_close_prices_range;
 use crate::data::yahoo_periods::{Interval, Range};
 use crate::optimizer::calculate_returns::{calculate_expected_returns, calculate_simple_returns};
+use crate::optimizer::covariance_matrix::calculate_covariance_matrix;
+use anyhow::Result;
+use yahoo_finance_api as yahoo;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let provider = yahoo::YahooConnector::new().unwrap();
     let symbols = vec!["AAPL", "MSFT", "GOOG"];
-    let prices = fetch_close_prices_range(
-        &provider,
-        &symbols,
-        Interval::Day1,
-        Range::Month1,
-    ).await?;
+    let prices =
+        fetch_close_prices_range(&provider, &symbols, Interval::Day1, Range::Month1).await?;
     // let returns = calculate_simple_returns(prices);
-    let returns = calculate_expected_returns(prices);
-    println!("{:?}", returns);
+    // let returns = calculate_expected_returns(prices);
+    // println!("{:?}", returns);
+    let cov_matrix = calculate_covariance_matrix(prices).unwrap();
+    println!("{:?}", cov_matrix);
     Ok(())
 }
